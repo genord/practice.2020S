b2fef68fb8535f3d0cc4fe5ede5a733d3999d888
Арсен Микитенко <a.mikitenko@student.csn.khai.edu>
2020-08-20 22:04:16 +0300

    step5(releaseA)

--- timetask/timetask.cpp
+++ timetask/timetask.cpp
@@ -5,32 +5,33 @@
 
 int hours, minutes;
 
-int check1()
+void check1()
 {
-    if (hours > 24 || hours < 0) {
-        printf("ERROR");
-        _Exit(0);
+    if (hours > 24 || hours < 0 || hours == NULL) {
+       printf("ERROR");
+       _Exit(0);
     }
 }
 
 int check2()
 {
-    if (minutes > 60 || minutes < 0) {
+    if (minutes > 60 || minutes < 0 || minutes == NULL) {
         printf("ERROR");
         _Exit(0);
     }
 }
 
 int main()
-{
+{   
     setlocale(LC_ALL, "ukr");
     printf("Введiть час");
     printf("\nГодин:");
     scanf_s("%d", &hours);
+    check1();
     printf("Хвилинн:");
     scanf_s("%d", &minutes);
-    check1();
     check2();
     hoursconvert(hours);
     minutesconvert(minutes);
+    return(0);
 }
