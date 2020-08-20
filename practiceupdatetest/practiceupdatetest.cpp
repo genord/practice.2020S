813432d1740ac8e564e5e722cf4bbe6a0d8ef811
Арсен Микитенко <a.mikitenko@student.csn.khai.edu>
2020-08-20 23:28:59 +0300

    1

--- practiceupdatetest/practiceupdatetest.cpp
+++ practiceupdatetest/practiceupdatetest.cpp
@@ -1,1 +1,25 @@
-﻿aka obama
+﻿#include <stdio.h>
+#include <clocale>
+#include <stdlib.h>
+#include "timelib.h"
+
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "ukr");
+    printf("Введiть час");
+    printf("\nГодин:");
+    scanf_s("%d", &hours);
+
+    check1();
+    
+    printf("Хвилинн:");
+    scanf_s("%d", &minutes);
+    check2();
+    hoursconvert(hours);
+    minutesconvert(minutes);
+    check3();
+    return(0);
+}
