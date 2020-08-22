3c2ff08b082f274db77ceae4a9f51e09b010d6b0
Арсен Микитенко <a.mikitenko@student.csn.khai.edu>
2020-08-22 16:07:10 +0300

    Новые тесты

---
+++ testingBR/testingBR.cpp
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+#include <clocale>
+#include <stdlib.h>
+#include "prov.h"
+#define global const char  h, m;
+
+
+void check1()
+{
+    if (h > 24 || h < 0) {
+        printf("ERRORhr");
+        exit(1);
+    }
+}
+
+int check2()
+{
+    if (m > 60 || m < 0) {
+        printf("ERRORmin");
+        exit(1);
+    }
+}
+
+
+
+
+
+int main()
+{
+
+    setlocale(LC_ALL, "ukr");
+    printf("Введiть час");
+    printf("\nГодин:");
+    //scanf_s("%d", &h);  
+    check1();
+    printf("Хвилинн:");
+    // scanf_s("%d", &m);
+    check2();
+    hconvert(h);
+    mconvert(m);
+    return(0);
+}
