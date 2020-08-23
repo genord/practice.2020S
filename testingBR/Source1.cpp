55ef6b616cae0727fc49154eeae0ab396e6caa0d
Арсен Микитенко <a.mikitenko@student.csn.khai.edu>
2020-08-23 23:39:18 +0300

    test

---
+++ testingBR/Source1.cpp
@@ -0,0 +1,19 @@
+void check1() {
+
+    if (h > 24 || h < 0) {
+        printf("ERRORhr");
+        exit(1);
+    }
+}
+
+int check2()
+{
+    if (m > 60 || m < 0) {
+        printf("ERRORmin");
+        exit(1);
+    }
+}
+
+
+
+
--- testingBR/testingBR.cpp
+++ testingBR/testingBR.cpp
@@ -1,41 +1,41 @@
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 #include <clocale>
 #include "prov.h"
 #include <stdlib.h>
+int h = 99, m = 99;
 
-
-void check1()
-{
+void check1(){
     if (h > 24 || h < 0) {
         printf("ERRORhr");
         exit(1);
     }
 }
 
-int check2()
-{
+void check2(){
     if (m > 60 || m < 0) {
         printf("ERRORmin");
-        exit(1);
-    }
-}
+        exit(1);}}
 
 
 
 
 
-int main()
-{
+int main(){
 
-    setlocale(LC_ALL, "ukr");
-    printf("Введiть час");
-    printf("\nГодин:");
-    //scanf_s("%d", &h);  
+
+    setlocale(LC_ALL, "ru");
+    printf("Ввежите время");
+    printf("\nЧасы:");
+    scanf_s("%2d", &h);  
     check1();
-    printf("Хвилинн:");
-    // scanf_s("%d", &m);
+    if (m == 99) {
+        printf("\n999999999999999");
+    };
+    scanf_s("%2d", &m);
     check2();
     hconvert(h);
     mconvert(m);
     return(0);
+
 }
