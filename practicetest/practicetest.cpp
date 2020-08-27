10fd797f849b92021c0b69197c4a434181845525
Арсен Микитенко <a.mikitenko@student.csn.khai.edu>
2020-08-27 19:30:24 +0300

    1

--- practicetest/practicetest.cpp
+++ practicetest/practicetest.cpp
@@ -1,20 +1,63 @@
-﻿// practicetest.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <stdio.h>
+#include <clocale>
+#include <stdlib.h>
+#include "timefunct.h"
 
-#include <iostream>
+int hours, minutes;
+
+void check1()
+{
+    int res;
+    do {
+        res = scanf_s("%d", &hours);
+        while (getchar() != '\n');
+        if (res == 1) {}
+        else
+        {
+            printf("ERROR");
+            _Exit(0);
+        };
+    } while (res != 1);
+    if (hours > 24 || hours < 0)
+    {
+        printf("ERROR");
+        _Exit(0);
+    }
+
+}
+
+
+
+void check2()
+{
+    int res;
+    do {
+        res = scanf_s("%d", &minutes);
+        while (getchar() != '\n');
+        if (res == 1) {}
+        else
+        {
+            printf("ERROR");
+            _Exit(0);
+        };
+    } while (res != 1);
+    if (minutes > 60 || minutes < 0)
+    {
+        printf("ERROR");
+        _Exit(0);
+    }
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "ukr");
+    printf("Введiть час");
+    printf("\nГодин:");
+    check1();
+    printf("Хвилинн:");
+    check2();
+    hoursconvert(hours);
+    minutesconvert(minutes);
+    return(0);
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
